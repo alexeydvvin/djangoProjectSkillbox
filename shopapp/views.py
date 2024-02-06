12d087442c6e3081b4ef9eac4d196f9b7a31af90
Alexey Dubrovin <alexey.dubrovvin.gh@gmail.com>
2024-02-06 15:05:14 +0300

    shop_index return HttpRespone()*

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def shop_index(request: HttpRequest):
+    print(request.path)
+    print(request.method)
+    print(request.headers)
+    return HttpResponse("<h1>Hello World!</h1>")
